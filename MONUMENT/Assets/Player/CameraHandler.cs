82acbf86eb6d6c0aaadd59a15fdd9372ebcdd8ca
Dan-Mechanics <github.tim@desmidt.org>
2025-02-20 15:39:29 +0100

    added player

---
+++ MONUMENT/Assets/Player/CameraHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace MONUMENT
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        [SerializeField] private Transform eyes = null;
+         
+        private Vector3 pos;
+        private Vector3 vel;
+        private float time;
+
+        private void Update()
+        {
+            transform.SetPositionAndRotation(pos + (vel * (Time.time - time)), eyes.rotation);
+        }
+
+        public void Refresh(Vector3 pos, Vector3 vel, float time)
+        {
+            this.pos = pos;
+            this.vel = vel;
+            this.time = time;
+        }
+    }
+}
