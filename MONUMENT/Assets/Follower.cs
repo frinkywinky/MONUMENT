16ba0cad201be6605eeb2ac908d8b3fdfcd4101a
Dan-Mechanics <github.tim@desmidt.org>
2025-03-19 10:37:02 +0100

    spirit object

---
+++ MONUMENT/Assets/Follower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PROJECTNAME
+{
+    /// <summary>
+    /// Rigidbody that goes towards player.
+    /// </summary>
+    public class Follower : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
